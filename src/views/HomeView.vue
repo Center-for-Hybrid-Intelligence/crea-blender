888b1143d488e4acec92447ebccb09527e6548f6
Safinaz Büyükgüzel <42682032+safinazbg@users.noreply.github.com>
2023-05-24 10:39:39 +0200

    prep

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,14 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    Hello Pieter and Eske :)
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  components: {}
 }
 </script>
