d3642b6dd00a7b40d6d1873af1ba66eea47b2fa0
Eske Junker <eskej@live.dk>
2023-06-01 14:55:43 +0200

    updates

--- src/views/ChatPDF.vue
+++ src/views/ChatPDF.vue
@@ -1,10 +1,8 @@
 <template>
   <!--select or drop single or multiple files form using tailwindcss and compositionAPI-->
-  <div class="flex flex-col items-center justify-center h-screen w-screen"
+  <div class="flex flex-col items-center justify-center w-screen mt-32"
   >
-
-
-    <div class="flex flex-col gap-8   items-center ">
+    <div class="flex flex-col gap-8 items-center ">
 
       <label class="block text-7xl font-bold text-gray-700" for="files">
         Upload PDF File
@@ -60,15 +58,55 @@
       </div>
 
     </div>
-    <div class="">
-      <div v-for="(file, index) in files" :key="index">
-        <div class="flex flex-col my-2">
-          <img :src="file" class="w-40 h-40 object-contain"/>
-          <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" @click="deleteFile(index)">
+    <transition-group name="list" class="flex flex-wrap justify-center gap-4" tag="div">
+      <div v-for="(file, index) in files" :key="index" class="mb-4 w-5/12 ">
+        <div class="flex my-2 gap-4 justify-between m-5 p-4 shadow-2xl w-full shadow-gray-300 rounded-xl ">
+          <img src="pdf-placeholder.png" class="h-full w-16 object-contain place-self-center"/>
+          <div class="flex flex-col w-full ">
+            <h1 class="text-start self-start pb-2" style="
+    display: -webkit-box;
+    -webkit-line-clamp: 1;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+                text-overflow: ellipsis;
+"
+
+            >
+              {{ file.name }}
+            </h1>
+            <div class="flex align-middle h-full gap-2 ">
+              <!--              <h1 class="text-mg font-bold place-self-center  ">
+                                Author:
+                            </h1>-->
+              <input
+                  v-model="file.author"
+                  type="text"
+                  class="mt-1 w-full px-2 py-1 border-gray-300 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
+                  placeholder="Author's Name"
+              />
+            </div>
+          </div>
+          <button
+              class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded max-h-12 self-center place-self-center"
+              @click="deleteFile(index)">
             Delete
           </button>
         </div>
       </div>
+    </transition-group>
+
+    <div class="flex justify-center gap-6 m-6" v-if="files.length > 0">
+      <button @click="onsubmit()"
+              class="p-4 px-8 text-2xl font-bold bg-green-500 self-center rounded-xl text-white hover:bg-green-600 transition-all duration-300">
+        Submit
+      </button>
+      <input
+          v-model="inputFieldValue"
+          type="text"
+          placeholder="Enter ID"
+          class="px-2 py-1 border border-gray-400 rounded-l focus:outline-none"
+      />
+
     </div>
   </div>
 
@@ -89,6 +127,7 @@ export default {
   setup(props, {emit}) {
     const files = ref(props.value);
     const isDragging = ref(false);
+    const inputFieldValue = ref('');
 
     const handleDragEnter = (e) => {
       e.preventDefault();
@@ -108,34 +147,56 @@ export default {
       e.preventDefault();
       isDragging.value = false;
 
-      const files = e.dataTransfer.files;
-      const file = files[0];
-
-      // Perform further processing with the dropped file (e.g., upload or read the PDF file)
-      console.log('Dropped file:', file);
+      const droppedFiles = e.dataTransfer.files;
+      for (let i = 0; i < droppedFiles.length; i++) {
+        const file = droppedFiles[i];
+        if (file.type === "application/pdf") {
+          const reader = new FileReader();
+          reader.onload = (e) => {
+            files.value.push({
+              preview: e.target.result,
+              name: file.name,
+              author: "",
+            });
+          };
+          reader.readAsDataURL(file);
+          console.log(file.name, "file")
+          console.log(files, "file")
+        }
+      }
+      emit("update:modelValue", files.value);
     };
 
     const onFileChange = (e) => {
-      const files = e.target.files || e.dataTransfer.files;
-      if (!files.length)
-        return;
-      createImage(files);
-    }
-
-    const createImage = (file) => {
-      let reader = new FileReader();
-      reader.onload = (e) => {
-        files.value.push(e.target.result);
-      };
-      reader.readAsDataURL(file[0]);
+      const selectedFiles = e.target.files;
+      for (let i = 0; i < selectedFiles.length; i++) {
+        const file = selectedFiles[i];
+        if (file.type === "application/pdf") {
+          const reader = new FileReader();
+          reader.onload = (e) => {
+            files.value.push({
+              preview: e.target.result,
+              name: file.name,
+              author: "",
+            });
+          };
+          reader.readAsDataURL(file);
+        }
+      }
       emit("update:modelValue", files.value);
-    }
+    };
 
+
+    const onsubmit = () => {
+      // Handle form submission with inputFieldValue and files.value
+
+      console.log("Input Field Value:", inputFieldValue.value);
+      console.log("Uploaded Files:", files.value);
+    };
     const deleteFile = (index) => {
       files.value.splice(index, 1);
       emit("update:modelValue", files.value);
-    }
-
+    };
     return {
       files,
       onFileChange,
@@ -144,12 +205,23 @@ export default {
       handleDragEnter,
       handleDragOver,
       handleDragLeave,
-      handleDrop 
+      handleDrop,
+      onsubmit,
+      inputFieldValue
     }
   }
 }
 </script>
 
 <style scoped>
+.list-enter-active {
+  transition: all 1s ease;
+}
+
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+  transform: translateX(50px);
+}
 
 </style>
