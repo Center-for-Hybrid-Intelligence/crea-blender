26323e08fe2ef9344c39ff81e6dc7e49c76406c9
Eske Junker <eskej@live.dk>
2023-06-15 13:27:32 +0200

    pushing

--- src/components/TileGallery.vue
+++ src/components/TileGallery.vue
@@ -1,8 +1,8 @@
 <template>
-  <div v-if="visible" class="absolute  w-11/12 k1:w-3/5 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-slate-100 rounded-3xl shadow-lg z-50 overflow-x-hidden overflow-y-scroll h-5/6 ">
-    <h1 class="heading1  text-center w-full !font-bold">Your shapes so far</h1>
+  <div v-if="visible" class="absolute  w-11/12 k1:w-3/5 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-slate-100 rounded-3xl shadow-lg z-50 overflow-x-hidden  h-5/6 ">
+    <h1 class="heading1  text-center w-full !font-bold">Gallery</h1>
     <div class="flex justify-center">
-      <button class="bg-black absolute top-0 right-0 rounded-full text-white w-12 h-12 font-bold text-2xl p-2 m-2 hover" @click="$emit('close')">X</button>
+      <button class="bg-black absolute top-0 right-0 rounded-full text-white w-12 h-12 font-bold text-2xl p-2 m-8 hover" @click="$emit('close')">X</button>
     </div>
     <div class="grid k1:grid-cols-4 h6:grid-cols-3 grid-cols-2 gap-4 k15:gap-8 p-8 justify-center">
       <div v-for="shape in createdShapes" :key="shape" class= "bg-white rounded-xl  ">
