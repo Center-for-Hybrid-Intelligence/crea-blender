0fbc3d4841e61051a87cd731a07efff9aa92e57c
Pieterwerff <pieterwerff1@gmail.com>
2023-06-28 13:48:49 +0200

    small fix

--- src/components/Tiles/Challenge/Tiles4TileGrids.vue
+++ src/components/Tiles/Challenge/Tiles4TileGrids.vue
@@ -2,16 +2,16 @@
   <div class=" flex k1:flex-row flex-col flex-col-reverse p-8 align-middle min-h-screen justify-center">
     <div class="grid grid-cols-2 border-black gap-12  ">
       <div class="hover border-2 border-white min-w-min cursor-pointer self-end justify-center  flex  ">
-        <TileGrid class="" :gap="2" :tile-size="16" :grid-size="10" :editable="false" />
+        <TileGrid class="" :gap="3" :tile-size="16" :grid-size="10" :border-radius="0.2" :border-size="0.2" :editable="false" />
       </div>
       <div class="hover border-2 border-white min-w-min cursor-pointer self-end justify-center  flex  ">
-        <TileGrid class="" :gap="2" :tile-size="16" :grid-size="10" :editable="false"/>
+        <TileGrid class="" :gap="3" :tile-size="16" :grid-size="10" :border-radius="0.2" :border-size="0.2" :editable="false"/>
       </div>
       <div class="hover border-2 border-white min-w-min cursor-pointer self-start flex  justify-center">
-        <TileGrid class="" :gap="2" :tile-size="16" :grid-size="10" :editable="false"/>
+        <TileGrid class="" :gap="3" :tile-size="16" :grid-size="10" :border-radius="0.2" :border-size="0.2" :editable="false"/>
       </div>
       <div class="hover border-2 border-white min-w-min cursor-pointer self-start flex  justify-center">
-        <TileGrid class="" :gap="2" :tile-size="16" :grid-size="10" :editable="false"/>
+        <TileGrid class="" :gap="3" :tile-size="16" :grid-size="10" :border-radius="0.2" :border-size="0.2" :editable="false"/>
       </div>
     </div>
 
